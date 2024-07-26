cb7747bbf3521069785946376fd26d9bb8068383
BrunoAZCONEX <bruno.reis@azconex.com.br>
2024-07-26 15:54:42 -0300

    Update resolution.py

--- resolution.py
+++ resolution.py
@@ -1,35 +1,54 @@
 import csv
 
 # Função para filtrar os dados
+
+
 def filtrar_valores_interessantes(arquivo_csv, valorDeInteresse):
     """ Leitura das informações e armazenamento na lista """
-    
+
     valores_interessantes = []
+    grupo_atual = []
 
     with open(arquivo_csv, 'r') as csv_file:
         reader = csv.reader(csv_file)
         header = next(reader)  # Ler o cabeçalho, se houver
-        
+
         for linha in reader:
-            # Supondo que a coluna de interesse seja a primeira (índice 0)
-            # print(linha[7].index(","))
-            # linha[7].remove(",")
-            # print(linha[7])
-            # break
-            valorDoBanco = float(linha[7])  # Converter para float
-            
+            # Definindo as informações
+            grupo = linha[0]
+            valor = linha[7]
+            parcela = linha[8]
+            contemplacao = linha[11]
+
+            # Converter para float e formatando
+            valorDoBanco = float(valor.replace(",", ""))
+
             # Verificar se o valorDoBanco está no intervalo desejado
-            if valorDeInteresse <= valorDoBanco <= 1.1 * valorDeInteresse:
-                valores_interessantes.append(valorDoBanco)
+            if (valorDeInteresse <= valorDoBanco and valorDeInteresse * 1.1 >= valorDoBanco):
+                # todas as informações desejadas estão o no grupo atual
+                grupo_atual = [grupo, valor, parcela, contemplacao]
+                valores_interessantes.append(grupo_atual)
 
-    return valores_interessantes
+        if valores_interessantes:
+            return valores_interessantes
+        else:
+            return None
 
-# Exemplo de uso
-arquivo_csv = 'banco.csv'
-valorDeInteresse = 1000  # Valor de interesse definido pelo cliente
-valores_interessantes = filtrar_valores_interessantes(arquivo_csv, valorDeInteresse)
 
-# Exibir os resultados
-print("Valores interessantes encontrados:")
-for valor in valores_interessantes:
-    print(valor)
+def main():
+    # Exemplo de uso
+    arquivo_csv = 'banco.csv'
+    valorDeInteresse = 70000  # Valor de interesse definido pelo cliente
+    valores_interessantes = filtrar_valores_interessantes(
+        arquivo_csv, float(valorDeInteresse))
+
+    # Exibir os resultados
+    if valores_interessantes != None:
+        print("Valores interessantes encontrados:")
+        for dado in valores_interessantes:
+            print(dado)
+    else:
+        print("Valores não encontrados")
+
+if __name__ == "__main__":
+    main()
